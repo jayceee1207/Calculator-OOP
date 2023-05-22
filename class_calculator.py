8191ba6f8b1e5a736d7305f6b454c4154d98b216
jayceee1207 <ablayjc7@gmail.com>
2023-05-22 13:03:39 +0800

    Pseudocode

---
+++ class_calculator.py
@@ -0,0 +1,13 @@
+#Make a class calculator with the functions
+
+    #Title function
+
+    #Message function
+
+    #Input two numbers
+
+    #Perform operation
+
+    #Display Result
+
+    #Thank you message
