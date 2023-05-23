ecb09119e53d37e33b4c0dd0af99d31e0ebdbab5
jayceee1207 <ablayjc7@gmail.com>
2023-05-23 16:24:29 +0800

    try again class

--- tryagain_calculator.py
+++ tryagain_calculator.py
@@ -8,14 +8,18 @@ class TryAgain:
         self.result_button = result_button
         self.number1_entry = number1_entry
         self.number2_entry = number2_entry
+    
     #function for messagebox
-
-    #ask user
-
-    #if answer is yes
-         
-        #the program will rerun
-
-    #if the user answers no
-
-        #the program will end
+    def ask(self):   
+        #ask user
+        moredata = moredata.askquestion("Do you want to try again?")
+        #if answer is yes
+        if moredata == "Yes":
+            #the program will rerun
+            self.result_button.config(text="Result")
+            self.number1_entry.delete(0, "end")
+            self.number2_entry.delete(0, "end")
+        #else
+            #the program will end
+            messagebox.showinfo("Thank you for using the calculator!")
+            self.root.destroy()
