36685efb6359b97eaf3b5fd06d9ec59aabcd115a
jayceee1207 <ablayjc7@gmail.com>
2023-05-23 17:24:25 +0800

    GUI Tkinter

--- Tkinter GUI.py
+++ Tkinter GUI.py
@@ -1,3 +1,25 @@
 #import tkinter
     
 #Class for User interface
+
+    #Init frame
+
+    #Create widgets
+
+        #widget for 1 label
+
+        #widget for 1 entry
+
+        #widget for 2 label
+
+        #widget for 2 entry
+
+        #widget for operator
+
+        #initial value is operator
+
+        #create a menu functions
+
+        #calculations
+
+        #display result
