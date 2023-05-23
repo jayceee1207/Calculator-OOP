bd87119b9edbd4cfe6dc38a7c2c6a441cf73e91f
jayceee1207 <ablayjc7@gmail.com>
2023-05-23 16:16:52 +0800

    attributes for try again

--- tryagain_calculator.py
+++ tryagain_calculator.py
@@ -1,9 +1,13 @@
 #import tkinter messagebox
-
+from tkinter import messagebox
 #create a class try again
-
+class TryAgain:
     #attributes for the class
-
+    def __init__ (self, root, result_button, number1_entry, number2_entry):
+        self.root = root
+        self.result_button = result_button
+        self.number1_entry = number1_entry
+        self.number2_entry = number2_entry
     #function for messagebox
 
     #ask user
