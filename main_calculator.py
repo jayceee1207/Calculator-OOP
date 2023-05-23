b7adb7a79d65268327dcd1789f334dd7366f5c66
jayceee1207 <ablayjc7@gmail.com>
2023-05-23 18:21:15 +0800

    Create window for calculator

--- main_calculator.py
+++ main_calculator.py
@@ -4,5 +4,14 @@ from TkinterGUI import UserInterface
 #import tkinter for window
 from tkinter import *
 
+# Create the window
+window = Tk()
+window.title("Simple Calculator")
+window.geometry("400x200")
+
+# Call the User Interface
+app = UserInterface(window)
+
+window.mainloop()
 
 
