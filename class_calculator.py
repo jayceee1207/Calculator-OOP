d462e965a146c23cbe7ace6659a636b7ac021e79
jayceee1207 <ablayjc7@gmail.com>
2023-05-23 15:35:01 +0800

    Menu for operations

--- class_calculator.py
+++ class_calculator.py
@@ -4,6 +4,9 @@ import tkinter as tk
 class Calculator:
 
 #Create window for calculator
+    def __init__(self, calculator):
+        self.calculator = calculator
+        self.window
 
     #Window format
     def window(self):
@@ -11,10 +14,23 @@ class Calculator:
         self.root.title("Simple App Calculator")
         #Get two numbers from the user
 
-        self.number_1_label = tk.Label(self.root, text="Please enter first number: ")
-        self.number_2_label = tk.Label(self.root, text="Please enter second number: ")
+        self.number1_label = tk.Label(self.root, text="Please enter first number: ")
+        self.number2_label = tk.Label(self.root, text="Please enter second number: ")
+
+        self.number1_entry = tk.Entry(self.root)
+        self.number2_entry = tk.Entry(self.root)
+
+        #Grid and label for number 1 and 2. sticky means we will put the label on the east side or right side
+        self.number1_label.grid(row=0, column=0, sticky="e")
+        self.number2_label.grid(row=1, column=0, sticky="e")
+
+        self.number1_entry.grid(row=0, column=1)
+        self.number2_entry.grid(row=1, column=1)
         #Choose operation
 
+        self.operation_var = tk.StringVar(value = "Choose operation")
+
+        #make a menu where the user will choose any operation from list
         #Button for user input
 
         #Button for operation
--- main_calculator.py
+++ main_calculator.py
@@ -1,19 +1,6 @@
 #import Calculator
-from class_calculator import Calculator
+
 #Call function from Calculator class
 
 
 
-
-calc = Calculator()
-
-#print title
-calc.title()
-
-#print message for the user
-calc.message()
-
-#call the menu_calculator
-
-calc.menu_calculator()
-    
