f3daa1a8e0497477249d38ffa5e97aca3aa0e516
jayceee1207 <ablayjc7@gmail.com>
2023-06-03 22:57:10 +0800

    Make a class for remainders of two numbers

--- new_operations_calculator.py
+++ new_operations_calculator.py
@@ -1,5 +1,10 @@
 #import operations from operations_calculator
 from operations_calculator import operations
-#make a class for number to round off if they want to
 
 
+#make a class for number to get the remainder of two numbers
+
+
+class RemainderNumbers(operations):
+    def remainder(self, number1, number2):
+        return number1 % number2
